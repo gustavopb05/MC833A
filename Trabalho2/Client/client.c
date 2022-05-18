988c2303408a97c4fed461f07669b3db7b9e9ee1
Gustavo Praciano Barros <ra217555@gambit.lab.ic.unicamp.br>
2022-05-18 09:46:02 -0300

    RequestInfo send feito, falta receive

--- Trabalho2/Client/client.c
+++ Trabalho2/Client/client.c
@@ -1,5 +1,5 @@
 /*
-** talker.c -- a datagram "client" demo
+** client.c -- a stream socket client demo
 */
 
 #include <stdio.h>
@@ -7,23 +7,238 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/time.h>
 
-#define SERVERPORT "4950"	// the port users will be connecting to
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[])
+#define PORT "3490" // the port client will be connecting to 
+
+#define MAXDATASIZE 1000 // max number of bytes we can get at once 
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
 {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+//#########################################
+// Funções que trocam mensagem com o servidor
+
+void requestInfo(int sockfd,char *send_message, struct addrinfo *p) {
+
+	int numbytes;
+	char buf[MAXDATASIZE];
+	struct timeval tv1, tv2;
+
+	gettimeofday(&tv1, NULL);
+	if ((numbytes = sendto(sockfd, send_message, strlen(send_message), 0,
+			 p->ai_addr, p->ai_addrlen)) == -1) {
+		perror("talker: sendto");
+		exit(1);
+	}
+
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+	gettimeofday(&tv2, NULL);
+	buf[numbytes] = '\0';
+
+	printf("%s\n",buf);
+	printf("TT = %06ld\n", tv2.tv_usec - tv1.tv_usec);
+}
+
+void requestRegister(int sockfd, char *send_message) {
+
+	int numbytes, i = 0;
+	char buf[MAXDATASIZE];
+	char movie[1000] = "";
+
+	if (send(sockfd, send_message, strlen(send_message), 0) == -1)
+		perror("send");
+
+	char infos[100];
+
+	printf("Digite o título:\n");
+
+	infos[i] = getchar();    /* get the first character */
+	while( infos[i] != '\n' ){
+		infos[++i] = getchar(); /* gets the next character */
+	}
+	infos[i] = '\0';
+	i = 0;
+
+	if (send(sockfd, infos, strlen(infos), 0) == -1)
+		perror("send");
+
+	strcat(movie, infos);
+	strcat(movie, "\n");
+	
+	printf("Digite o genero:\n");
+	infos[i] = getchar();    /* get the first character */
+	while( infos[i] != '\n' ){
+		infos[++i] = getchar(); /* gets the next character */
+	}
+	infos[i] = '\0';
+	i = 0;
+
+	strcat(movie, infos);
+	strcat(movie, "\n");
+	
+	printf("Digite o diretor:\n");
+	infos[i] = getchar();    /* get the first character */
+	while( infos[i] != '\n' ){
+		infos[++i] = getchar(); /* gets the next character */
+	}
+	infos[i] = '\0';
+	i = 0;
+
+	strcat(movie, infos);
+	strcat(movie, "\n");
+
+	printf("Digite o ano:\n");
+	infos[i] = getchar();    /* get the first character */
+	while( infos[i] != '\n' ){
+		infos[++i] = getchar(); /* gets the next character */
+	}
+	infos[i] = '\0';
+	i = 0;
+
+	strcat(movie, infos);
+	strcat(movie, "\n\0");
+
+	if (send(sockfd, movie, strlen(movie), 0) == -1)
+		perror("send");
+	
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+
+	buf[numbytes] = '\0';
+}
+
+void requestMovieId(int sockfd, char *send_message) {
+	int numbytes, i = 0;
+	char buf[MAXDATASIZE];
+	char idChar[3] = "";
+
+	if (send(sockfd, send_message, strlen(send_message), 0) == -1)
+		perror("send");
+
+	printf("Digite o id:\n");
+
+	idChar[i] = getchar();    /* get the first character */
+	while( idChar[i] != '\n' ){
+		idChar[++i] = getchar(); /* gets the next character */
+	}
+	idChar[i] = '\0';
+	i = 0;
+
+	if (send(sockfd, idChar, strlen(idChar), 0) == -1)
+		perror("send");
+
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+
+	buf[numbytes] = '\0';
+	printf("%s\n",buf);
+}
+
+void requestAcrescentaGen(int sockfd, char *send_message){
+	int numbytes, i = 0;
+	char buf[MAXDATASIZE];
+	char sChar[100] = "";
+
+	if (send(sockfd, send_message, strlen(send_message), 0) == -1)
+		perror("send");
+
+	printf("Digite o Título:\n");
+
+	sChar[i] = getchar();    /* get the first character */
+	while( sChar[i] != '\n' ){
+		sChar[++i] = getchar(); /* gets the next character */
+	}
+	sChar[i] = '\0';
+	i = 0;
+
+	if (send(sockfd, sChar, strlen(sChar), 0) == -1)
+		perror("send");
+
+	printf("Digite o Gênero:\n");
+
+	sChar[i] = getchar();    /* get the first character */
+	while( sChar[i] != '\n' ){
+		sChar[++i] = getchar(); /* gets the next character */
+	}
+	sChar[i] = '\0';
+	i = 0;
+
+	if (send(sockfd, sChar, strlen(sChar), 0) == -1)
+		perror("send");
+
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+
+	buf[numbytes] = '\0';
+	printf("%s\n",buf);
+}
+
+void requestListarGenAll(int sockfd, char *send_message) {
+	int numbytes, i = 0;
+	char buf[MAXDATASIZE];
+	char gChar[3] = "";
+
+	if (send(sockfd, send_message, strlen(send_message), 0) == -1)
+		perror("send");
+
+	printf("Digite o genero:\n");
+
+	gChar[i] = getchar();    /* get the first character */
+	while( gChar[i] != '\n' ){
+		gChar[++i] = getchar(); /* gets the next character */
+	}
+	gChar[i] = '\0';
+	i = 0;
+
+	printf("\n");
+
+	if (send(sockfd, gChar, strlen(gChar), 0) == -1)
+		perror("send");
+
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+
+	buf[numbytes] = '\0';
+	printf("%s\n",buf);
+}
+
+//##########################################
+// Função que abre a conexao e escolhe qual outra função pra enviar a mensagem
+
+int sendServer(int argc, char *argv[], char *send_message) {
 	int sockfd;
 	struct addrinfo hints, *servinfo, *p;
 	int rv;
 	int numbytes;
 
-	if (argc != 3) {
-		fprintf(stderr,"usage: talker hostname message\n");
+	if (argc != 2) {
+		fprintf(stderr,"usage: talker hostname\n");
 		exit(1);
 	}
 
@@ -52,16 +267,68 @@ int main(int argc, char *argv[])
 		return 2;
 	}
 
-	if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
-			 p->ai_addr, p->ai_addrlen)) == -1) {
-		perror("talker: sendto");
-		exit(1);
+	//###################################
+	// Escolhe a função baseado na entrada
+
+	if (strcmp(send_message,"listAll") == 0 || strcmp(send_message,"listId") == 0)
+	{
+		requestInfo(sockfd, send_message, p);
 	}
+	else if (strcmp(send_message, "movieId") == 0 || strcmp(send_message, "removeId") == 0)
+	{
+		requestMovieId(sockfd, send_message);	
+	}
+	else if(strcmp(send_message, "acrescentaGen") == 0)
+	{
+		requestAcrescentaGen(sockfd, send_message);
+	}
+	else if(strcmp(send_message, "listarGenAll") == 0)
+	{
+		requestListarGenAll(sockfd, send_message);
+	}
+	else if (strcmp(send_message,"cadastrar") == 0)
+	{
+		requestRegister(sockfd, send_message);
+	}
+	else
+	{
+		requestInfo(sockfd, send_message, p);
+	}
+	
 
 	freeaddrinfo(servinfo);
 
-	printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
 	close(sockfd);
-
+	
 	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+
+	char message[MAXDATASIZE];
+	int ret, i =0;
+
+	while (1)
+	{
+		printf("\nAções disponíveis:\ncadastrar - Cadastra um novo filme\nlistAll - Lista todas informações de todos os filmes\nlistId - Lista todos os títulos e seu id\nmovieId - Infos de um filme dado seu id\nremoveId - Remove um filme pelo id\nacrescentaGen - Acescenta um gênero a um filme\nlistarGenAll - Lista todas as informações dos filmes de um gênero\n\nDigite uma ação:\n\n");
+
+		message[i] = getchar();    /* get the first character */
+		while( message[i] != '\n' ){
+			message[++i] = getchar(); /* gets the next character */
+		}
+		message[i] = '\0';
+		i = 0;
+
+		printf("\n");
+	
+		if(strcmp(message,"exit") == 0) {
+			break;
+		}
+		else {
+			ret = sendServer(argc, argv, message);
+		}
+	}
+	return ret;
 }
--- Trabalho2/Server/server.c
+++ Trabalho2/Server/server.c
@@ -1,5 +1,5 @@
 /*
-** listener.c -- a datagram sockets "server" demo
+** server.c -- a stream socket server demo
 */
 
 #include <stdio.h>
@@ -10,12 +10,30 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/time.h>
 
-#define MYPORT "4950"	// the port users will be connecting to
+#define PORT "3490"  // the port users will be connecting to
 
-#define MAXBUFLEN 100
+#define BACKLOG 10	 // how many pending connections queue will hold
+
+#define MAXDATASIZE 1000 // max number of bytes we can get at once 
+
+void sigchld_handler(int s)
+{
+	(void)s; // quiet unused variable warning
+
+	// waitpid() might overwrite errno, so we save and restore it:
+	int saved_errno = errno;
+
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+
+	errno = saved_errno;
+}
+
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -27,23 +45,446 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+//#########################
+// Funções que se comunicam com o client
+void listAll(int new_fd, struct timeval tv1) {
+
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char result[1000] = "";
+	struct timeval tv2;
+
+	fp = fopen("Movies/movies.txt", "r");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		strcat(result, line);
+	}
+
+	fclose(fp);
+	if (line)
+		free(line);
+
+	gettimeofday(&tv2, NULL);
+	printf("TS = %06ld\n", tv2.tv_usec - tv1.tv_usec);
+	if (send(new_fd, result, strlen(result), 0) == -1) {
+		perror("send");
+	}
+
+}
+
+void registerMovie(int new_fd) {
+
+	int numbytes;
+	char buf[MAXDATASIZE];
+
+	if ((numbytes = recv(new_fd, buf, 1000-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+	buf[numbytes] = '\n';
+	buf[numbytes+1] = '\0';
+
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	fp = fopen("Movies/index.txt", "a");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	fprintf(fp,"%s", buf); //Titulo no index
+
+	fclose(fp);
+	if (line)
+		free(line);
+
+	if ((numbytes = recv(new_fd, buf, 1000-1, 0)) == -1) { //Filme todo
+		perror("recv");
+		exit(1);
+	}
+	
+	fp = fopen("Movies/movies.txt", "a");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	fprintf(fp,"%s", buf); //Registra o filme
+
+	fclose(fp);
+	if (line)
+		free(line);
+
+	
+	fp = fopen("Movies/index.txt", "r");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	int idCadastro = 0;
+	while ((read = getline(&line, &len, fp)) != -1) { // Pega o número do cadastro
+		idCadastro++;
+	}
+
+	fclose(fp);
+	if (line)
+		free(line);
+
+	char idChar[3];
+	sprintf(idChar, "%d", idCadastro); //Passa int pra string
+
+	char result[100] = "\nCadastrado, id é: ";
+	strcat(result,idChar);
+
+	if (send(new_fd, result, strlen(result), 0) == -1) {
+		perror("send");
+	}
+
+}
+
+void listId(int new_fd, struct timeval tv1) {
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char result[1000] = "";
+	struct timeval tv2;
+
+	int id = 0;
+	char idChar[3];
+
+	fp = fopen("Movies/index.txt", "r");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		id++;
+
+		sprintf(idChar, "%d", id);
+		strcat(result, idChar);
+		strcat(result, " ");
+		strcat(result, line);
+
+	}
+
+	fclose(fp);
+	if (line)
+		free(line);
+
+	gettimeofday(&tv2, NULL);
+	printf("TS = %06ld\n", tv2.tv_usec - tv1.tv_usec);
+	if (send(new_fd, result, strlen(result), 0) == -1) {
+		perror("send");
+	}
+}
+
+void movieId(int new_fd) {
+
+	int numbytes;
+	char buf[MAXDATASIZE];
+
+	if ((numbytes = recv(new_fd, buf, 1000-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+	buf[numbytes] = '\0';
+
+	int idMovie = atoi(buf);
+
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char result[1000] = "";
+
+	int id = 0;
+	char idChar[3];
+
+	fp = fopen("Movies/movies.txt", "r");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		id++;
+
+		if (id >= (4*idMovie - 3) && id <= (4*idMovie)) {
+			strcat(result, line);
+		}
+	}
+
+	fclose(fp);
+	if (line)
+		free(line);
+
+	if (send(new_fd, result, strlen(result), 0) == -1) {
+		perror("send");
+	}
+}
+
+void removeId(int new_fd) {
+
+	int numbytes;
+	char buf[MAXDATASIZE];
+
+	if ((numbytes = recv(new_fd, buf, 1000-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+	buf[numbytes] = '\0';
+
+	int idMovie = atoi(buf);
+
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char result[1000] = "";
+
+	int id = 0;
+
+	fp = fopen("Movies/movies.txt", "r");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		id++;
+
+		if (!(id >= (4*idMovie - 3) && id <= (4*idMovie))) {
+			strcat(result, line);
+		}
+	}
+
+	id = 0;
+
+	fclose(fp);
+
+	fp = fopen("Movies/movies.txt", "w");
+	if (fp == NULL)
+		printf("Erro\n");
+	
+	fprintf(fp,"%s", result);
+
+	fclose(fp);
+
+
+	char result2[1000] = "";
+
+	fp = fopen("Movies/index.txt", "r");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		id++;
+
+		if (id != idMovie ) {
+			strcat(result2, line);
+		}
+	}
+
+	fclose(fp);
+
+	fp = fopen("Movies/index.txt", "w");
+	if (fp == NULL)
+		printf("Erro\n");
+	
+	fprintf(fp,"%s", result2);
+
+	fclose(fp);
+	if (line)
+		free(line);
+
+	char removed[100] = "Removido o filme ";
+	strcat(removed,buf);
+
+	if (send(new_fd, removed, strlen(removed), 0) == -1) {
+		perror("send");
+	}
+
+}
+
+// Acrescentar um novo gênero em um filme
+// Preciso, qual filme(titulo), qual gen 
+void acrescentaGen(int new_fd) {
+	
+	int numbytes;
+	char title[MAXDATASIZE];
+	char buf[MAXDATASIZE];
+
+	if ((numbytes = recv(new_fd, title, 1000-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+	title[numbytes] = '\n';
+	title[numbytes + 1] = '\0';
+
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char result[1000] = "";
+	int idMovie;
+	int found = 0;
+	int id = 0;
+
+	fp = fopen("Movies/index.txt", "r");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		id++;
+
+		if (strcmp(title,line) == 0) {
+			found = 1;
+			break;
+		}
+	}
+
+	fclose(fp);
+
+	if ((numbytes = recv(new_fd, buf, 1000-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+	buf[numbytes] = '\0';
+
+	if (found == 1) {
+		fp = fopen("Movies/movies.txt", "r");
+		if (fp == NULL)
+			printf("Erro\n");
+
+		idMovie = id;
+		id = 0 ;
+		while ((read = getline(&line, &len, fp)) != -1) {
+			id++;
+
+			if ( id == (4*idMovie - 2) ) {
+				line[strlen(line)-1] = '\0';
+
+				strcat(line, ", ");
+				strcat(line, buf);
+				strcat(line, "\n");
+				strcat(result, line);
+			}
+			else
+			{
+				strcat(result, line);
+			}
+		}
+
+		fclose(fp);
+
+		fp = fopen("Movies/movies.txt", "w");
+		if (fp == NULL)
+			printf("Erro\n");
+		
+		fprintf(fp,"%s", result);
+
+		fclose(fp);
+		if (line)
+			free(line);
+
+		char removed[100] = "Genero acrescentado no filme ";
+		strcat(removed,title);
+
+		if (send(new_fd, removed, strlen(removed), 0) == -1) {
+			perror("send");
+		}
+	}
+	else
+	{
+		char removed[100] = "Filme não encontrado";
+
+		if (send(new_fd, removed, strlen(removed), 0) == -1) {
+			perror("send");
+		}
+	}
+	
+
+}
+
+// Listar informações (título, diretor(a) e ano) de todos os filmes de um determinado gênero
+void listarGenAll(int new_fd) {
+	int numbytes;
+	char buf[MAXDATASIZE];
+
+	if ((numbytes = recv(new_fd, buf, 1000-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+	buf[numbytes] = '\0';
+
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char result[1000] = "";
+	int listaid[1000];
+	int idcont = 0;
+	int idincre = 0;
+	int readOneMovie = 0;
+
+	int id = 0;
+
+	fp = fopen("Movies/movies.txt", "r");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		id++;
+
+		if ( (id + 2)%4==0 && strstr(line, buf) != NULL ){
+			listaid[idcont] = (id + 2)/4;
+			idcont++;
+		}
+	}
+	fclose(fp);
+
+	fp = fopen("Movies/movies.txt", "r");
+	if (fp == NULL)
+		printf("Erro\n");
+
+	id = 0;
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		id++;
+
+		if (idincre < idcont && id >= (4*listaid[idincre] - 3) && id <= (4*listaid[idincre])) {
+			strcat(result, line);
+			readOneMovie++;
+			if (readOneMovie == 4)
+			{
+				readOneMovie = 0;
+				idincre++;
+			}
+		}
+	}
+	fclose(fp);
+
+	if (send(new_fd, result, strlen(result), 0) == -1) {
+		perror("send");
+	}
+	
+}
+
 int main(void)
 {
-	int sockfd;
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
 	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytes;
-	struct sockaddr_storage their_addr;
-	char buf[MAXBUFLEN];
-	socklen_t addr_len;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1;
 	char s[INET6_ADDRSTRLEN];
+	int rv;
 
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_INET; // set to AF_INET to use IPv4
-	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; // use my IP
 
-	if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
@@ -52,44 +493,113 @@ int main(void)
 	for(p = servinfo; p != NULL; p = p->ai_next) {
 		if ((sockfd = socket(p->ai_family, p->ai_socktype,
 				p->ai_protocol)) == -1) {
-			perror("listener: socket");
+			perror("server: socket");
 			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
 		}
 
 		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
 			close(sockfd);
-			perror("listener: bind");
+			perror("server: bind");
 			continue;
 		}
 
 		break;
 	}
 
-	if (p == NULL) {
-		fprintf(stderr, "listener: failed to bind socket\n");
-		return 2;
-	}
+	freeaddrinfo(servinfo); // all done with this structure
 
-	freeaddrinfo(servinfo);
-
-	printf("listener: waiting to recvfrom...\n");
-
-	addr_len = sizeof their_addr;
-	if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
-		(struct sockaddr *)&their_addr, &addr_len)) == -1) {
-		perror("recvfrom");
+	if (p == NULL)  {
+		fprintf(stderr, "server: failed to bind\n");
 		exit(1);
 	}
 
-	printf("listener: got packet from %s\n",
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+
+	while(1) {  // main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
 		inet_ntop(their_addr.ss_family,
 			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof s));
-	printf("listener: packet is %d bytes long\n", numbytes);
-	buf[numbytes] = '\0';
-	printf("listener: packet contains \"%s\"\n", buf);
+			s, sizeof s);
+		printf("server: got connection from %s\n", s);
 
-	close(sockfd);
+		if (!fork()) { // this is the child process
+			close(sockfd); // child doesn't need the listener
+
+			// ##############################
+			// Escolhe que função realizar
+			int numbytes;
+			char buf[1000];
+			struct timeval tv1;
+
+			if ((numbytes = recv(new_fd, buf, 1000-1, 0)) == -1) {
+				perror("recv");
+				exit(1);
+			}
+			gettimeofday(&tv1, NULL);
+
+			buf[numbytes] = '\0';
+			printf("%s\n",buf);
+
+			if (strcmp(buf,"listAll")==0) { // Lista todos os filmes
+				listAll(new_fd, tv1);
+			}
+			else if (strcmp(buf,"cadastrar")==0) { // Registra um filme
+				registerMovie(new_fd);
+			}
+			else if (strcmp(buf,"listId")==0) { // Lista todos os títulos e Id
+				listId(new_fd, tv1);
+			}
+			else if (strcmp(buf, "movieId") == 0) { // Lista infomações de um filme por Id
+				movieId(new_fd);
+			}
+			else if (strcmp(buf, "removeId") == 0) { // Remove um filme
+				removeId(new_fd);
+			}
+
+			else if (strcmp(buf, "acrescentaGen") == 0) { // Acrescentar um novo gênero em um filme
+				acrescentaGen(new_fd);
+			}
+			else if (strcmp(buf, "listarGenAll") == 0) { // Listar informações (título, diretor(a) e ano) de todos os filmes de um determinado gênero
+				listarGenAll(new_fd);
+			}
+			else {
+				char error[100] = "Não existe essa função";
+
+				if (send(new_fd, error, strlen(error), 0) == -1) {
+					perror("send");
+				}
+			}
+
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd);  // parent doesn't need this
+	}
 
 	return 0;
 }
